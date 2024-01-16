d5cd51eba0d7817cca7bb75161acc93aca504b9d
dechen:) <dechendb@gmail.com>
2024-01-16 10:46:49 -0500

    created the increase quantity, toString, and main method

--- BakedGood.java
+++ BakedGood.java
@@ -25,5 +25,44 @@ public class BakedGood
         this.quantity= quantity;
     }
     
+    /**
+     * Modify the quantity of goods: baking more --> increasing the number of goods
+     * 
+     *  param: the amount by which we are increasing the count
+     *  return: nothing
+     *  
+     */
+    public void IncreaseQuant (int count) {
+        this.quantity += count;
+    }
     
+    /**
+     * toString
+     * 
+     * param: nothing
+     * return: String representation of our work
+     */
+    public String toString(){
+        return "Baked Good" + this.name + " (" +this.quantity +","+ " @" + "$"+this.price+ ")";
+    }
+    
+    /**
+     * Main
+     * 
+     * Where we can test our menthods
+     */
+    public static void main(String[] args) {
+        BakedGood crossiant = new BakedGood ("crossiant", "butter, flour, bake, yummers", 20.0, 0);
+        
+        //bake a dozen more crossiants
+        crossiant.IncreaseQuant(12);
+        
+        System.out.println(crossiant);
+        
+        //bake 3 more crossiants with leftover dough
+        crossiant.IncreaseQuant(3);
+        System.out.println(crossiant);
+        
+        
+    }
 }
